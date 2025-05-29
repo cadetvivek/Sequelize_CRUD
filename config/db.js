4c2887ed91240522b5516209d3014042102c8314
cadetvivek <kushwahvivek805@gmail.com>
2025-05-29 17:21:18 +0530

    add migration

--- config/db.js
+++ config/db.js
@@ -7,9 +7,11 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
-    port: process.env.DB_PORT,
+    logging: false,
+    
   }
 );
 
